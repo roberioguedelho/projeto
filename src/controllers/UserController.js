742948b299cc7469ad2c3e241bbdc350ee817adb
Roberio Guedelho <roberiocrow@gmail.com>
2021-07-15 19:11:33 -0300

    Finishing User CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,79 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const users = await User.findByPk(req.params.id);
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não informado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User não existe'],
+        });
+      }
+
+      const userUpdate = await user.update(req.body);
+
+      return res.json(userUpdate);
+    } catch (e) {
+      return res.json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não informado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User não existe'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -5,5 +5,9 @@ import UserController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', UserController.store);
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
